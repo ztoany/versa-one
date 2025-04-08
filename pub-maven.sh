c6fe99735b3881c9ac9e6512f7b51c9ce6c41f46
ztoany <marvin.m.huang@outlook.com>
2025-04-08 19:39:27 +0800

    add pub-maven.sh

---
+++ pub-maven.sh
@@ -0,0 +1,13 @@
+gradle versa-infra-common:publishAndReleaseToMavenCentral --no-configuration-cache
+
+gradle versa-springboot:publishAndReleaseToMavenCentral --no-configuration-cache
+
+gradle versa-springboot-starters:localization-boot-starter:publishAndReleaseToMavenCentral --no-configuration-cache
+
+gradle versa-springboot-starters:jpa-boot-starter:publishAndReleaseToMavenCentral --no-configuration-cache
+
+gradle versa-springboot-starters:web-logging-boot-starter:publishAndReleaseToMavenCentral --no-configuration-cache
+
+gradle versa-springboot-starters:webmvc-problem-details-boot-starter:publishAndReleaseToMavenCentral --no-configuration-cache
+
+gradle versa-springboot-starters:webflux-problem-details-boot-starter:publishAndReleaseToMavenCentral --no-configuration-cache
