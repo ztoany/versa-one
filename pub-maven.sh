aa0f98f9413d4c5ecf2474a4a877eaf68005f694
ztoany <marvin.m.huang@outlook.com>
2025-04-13 18:49:08 +0800

    update pub-maven.sh

--- pub-maven.sh
+++ pub-maven.sh
@@ -1,5 +1,7 @@
 gradle versa-infra-common:publishAndReleaseToMavenCentral --no-configuration-cache
 
+gradle versa-business-common:publishAndReleaseToMavenCentral --no-configuration-cache
+
 gradle versa-springboot:publishAndReleaseToMavenCentral --no-configuration-cache
 
 gradle versa-springboot-starters:localization-boot-starter:publishAndReleaseToMavenCentral --no-configuration-cache
@@ -11,3 +13,7 @@ gradle versa-springboot-starters:web-logging-boot-starter:publishAndReleaseToMav
 gradle versa-springboot-starters:webmvc-problem-details-boot-starter:publishAndReleaseToMavenCentral --no-configuration-cache
 
 gradle versa-springboot-starters:webflux-problem-details-boot-starter:publishAndReleaseToMavenCentral --no-configuration-cache
+
+gradle versa-springboot-starters:business-service-boot-starter:publishAndReleaseToMavenCentral --no-configuration-cache
+
+gradle versa-springboot-starters:odata-query-olingo-jpa-boot-starter:publishAndReleaseToMavenCentral --no-configuration-cache
